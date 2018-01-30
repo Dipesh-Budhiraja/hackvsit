1bc220d6d376f3b70eb555a5a723c74041b9b4f7
Dipesh-Budhiraja <25dipesh98@gmail.com>
2018-01-30 12:57:57 +0530

    packages

--- app.js
+++ app.js
@@ -1,17 +1,33 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const ejs = require('ejs');
+const methodOverride = require('method-override');
+const flash = require('connect-flash');
+
+//import routes
+var vendor = require('./routes/vendor');
+
+//mongo connect
+mongoose.connect('mongodb://admin:admin@ds217898.mlab.com:17898/vending-machine');
 
 //app
 var app = express();
 
+//middleware
+app.use(bodyParser.urlencoded({extended: true}));
+app.set('view engine', 'ejs');
+app.use(express.static(__dirname + '/public'));
+app.use(methodOverride("_method"));
+app.use(flash());
+
+//routes
 app.get('/', function(req, res){
     res.send('home page');
 });
-//import routes
-var vendor = require('./routes/vendor');
 app.use('/vendor', vendor);
 
+//app listen
 var port = 3000;
 app.listen(process.env.PORT || port, function(err){
     console.log('server on ' + port);
