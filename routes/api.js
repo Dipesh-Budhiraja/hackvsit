d3606269f2ff8903c3bd40883a89474f46be4168
pratyush1687 <pratyush1687@gmail.com>
2018-01-31 07:05:41 +0530

    1234567890

--- routes/api.js
+++ routes/api.js
@@ -5,8 +5,8 @@
 const express = require('express');
 const router = express.Router();
 const Machine = require('../models/vending');
-
-
+const config = require('../config/config')
+const request = require('request')
 // router.get('/getData', function (req, res) {
 //     // let vendId=req.body.machineId;
 //     res.send({
@@ -55,8 +55,28 @@ router.post('/getData', (req, res) => {
             res.status(200).send(err);
         }
         if (machine) {
-            // console.log(machine);    
-            res.send(machine);
+            // console.log(machine);
+            request(
+                {
+                method:'post',
+                url:'http://192.168.136.217:5000/12', 
+                json: {user: "user97", products: {"Pringles": 0, "Lays": 0, "Pepsi": 2, "Mirinda": 0, "Snickers": 0, "Diary Milk Chocolate": 0, "Cadbury 5 Star": 0, "Sprite": 8, "Bingo Mad Angles": 0, "Kurkure Puffcorn": 0}, vendId: "vend123", availProds: ["Kurkure Puffcorn","Pringles","Dairy Milk Chocolate"]}, 
+                headers: {  
+                    "content-type": "application/json",
+                },
+                // json: true,
+            }, function (error, response, body) {  
+                //Print the Response
+                // console.log(response);
+                
+                console.log(body);  
+                // machine.re
+            // console.log(machine.refer);
+            // console.log(machine);
+            
+            res.send({machine:machine,refer:body});
+                
+        });     
         }
         else {
             res.send('code not found please scan again')
@@ -65,23 +85,48 @@ router.post('/getData', (req, res) => {
 })
 
 router.post('/transaction/:id',(req,res)=>{
-    console.log(req.body);
-    console.log(req.headers);
+    // console.log(req.body);
+    // console.log(req.headers);
     var vendId=req.params.id;
-    req.body=req.headers.json;
-    res.send('123');
+    console.log(vendId);
+    
+    cart=req.headers.json;
     Machine.findById(vendId,(err,machine)=>{
         tempProd = machine.products;
-
+        // console.log(tempProd);
+        cart=JSON.parse(cart);
+        cart=cart.cart;
+        var price=0;
+        // console.log(cart);
+        
         for(var i in tempProd){
-            for(var j in req.body.cart){
-                if(tempProd[i].ID==req.body.cart[j].id){
-                    // tempProd[]
-                    console.log('running')
-                    res.send('123');
+            // console.log(tempProd[i]);
+            
+            for(var j in cart){
+                // console.log(tempProd[i]);
+                // console.log(cart[j]);
+                
+                if(tempProd[i].ID==cart[j].id){
+                    var a=tempProd[i]
+                    a['Cur Quantity']=parseInt(a['Cur Quantity'])-parseInt(cart[j].quantity);
+                 console.log(a['Cur Quantity']);
+                 console.log(cart[j].quantity);
+                 price+=a.Price*cart[j].quantity;
+                 
+                    
                 }
             }
         }
+        machine.products=tempProd;
+        console.log(machine.products);
+        machine.save(function(){
+            date = new Date();
+            // res.redirec({
+                // console.log(vendId+price+date);
+                // console.log(date.getTime());
+                res.send({time:date.getTime(),price:price });
+            // })
+        });
     })
 })
 
